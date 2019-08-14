7461c393fff9e1a2af06cafe262fa09c1109bc3e
zenos <zenoslin@outlook.com>
2019-08-14 18:48:42 +0800

    feat: 代码重构

--- src/AtlasPlayer.js
+++ src/AtlasPlayer.js
@@ -1,77 +1,107 @@
-export const AtlasPlayer = async function($canvas, $imgUrl) {
-  if (!$canvas) {
-    console.log(`缺少指定canvas`);
-    return;
+export default class AtlasPlayer {
+  constructor(options) {
+    let _this = this;
+    if (!options) {
+      console.error("请设置参数！");
+      return;
+    }
+    // 是否初始化完成
+    this.initComplete = false;
+    this.queueFn = [];
+
+    // canvas
+    this.canvas = options.canvas;
+    // 图集图片
+    this.atlas = options.atlas;
+    // 图集JSON
+    this.json = options.json;
+
+    // 图集的加载DOM
+    this.imgDom = new Image();
+    // 循环播放Key
+    this.intervalKey = null;
+    // 帧集合
+    this.frames = null;
+    // 帧总数
+    this.framesNum = null;
+    // 二维渲染上下文
+    this.ctx = null;
+    // 帧率
+    this.fps = null;
+    // 当前帧
+    this.index = null;
+
+    this.init();
   }
-  let jsonPath = $imgUrl.replace(".png", ".json");
-  let json;
-  await loadJson(jsonPath)
-    .then(res => {
-      json = JSON.parse(res);
-    })
-    .catch(() => {
-      console.log(`找不到json文件`);
+
+  async init() {
+    if (!this.atlas) {
+      console.error("请指定图集！");
+      return;
+    }
+    await this.loadImg(this.atlas);
+
+    if (!this.json) {
+      console.error("缺少图集的JSON！");
+      return;
+    }
+    this.frames = this.json.frames;
+    this.framesNum = Object.keys(this.frames).length;
+
+    this.ctx = this.canvas.getContext("2d");
+    this.fps = 8;
+    this.index = 0;
+
+    if (this.frames["0.png"]) {
+      this.canvas.width = this.frames["0.png"].frame.w;
+      this.canvas.height = this.frames["0.png"].frame.h;
+    } else {
+      console.error(`缺少初始帧 0.png`);
+      return;
+    }
+    this.initComplete = true;
+    if (this.queueFn.length > 0) {
+      let _this = this;
+      this.queueFn.map(function(item) {
+        item.fn.call(_this, ...item.arguments);
+      });
+      this.queueFn = [];
+    }
+  }
+
+  play() {
+    if (!this.initComplete) {
+      this.queueFn.push({ fn: this.play, arguments: [] });
+      return;
+    }
+    let _this = this;
+    this.interval = setInterval(function() {
+      _this.ctx.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
+      let info = _this.frames[`${_this.index}.png`].frame;
+      _this.ctx.drawImage(
+        _this.imgDom,
+        info.x,
+        info.y,
+        info.w,
+        info.h,
+        0,
+        0,
+        _this.canvas.width,
+        _this.canvas.height
+      );
+      _this.index = _this.index < _this.framesNum - 1 ? _this.index + 1 : 0;
+    }, 1000 / _this.fps);
+  }
+
+  loadImg(imgUrl) {
+    return new Promise((resolve, reject) => {
+      this.imgDom.onload = function() {
+        resolve();
+      };
+      this.imgDom.onerror = function() {
+        reject(new Error(`无法加载图片${imgUrl}`));
+      };
+      this.imgDom.src = imgUrl;
     });
-  const imgDom = new Image();
-  await loadImg(imgDom, $imgUrl);
-
-  let frames = json.frames;
-  let keyArr = Object.keys(frames);
-  let framesNum = keyArr.length;
-
-  let ctx = $canvas.getContext("2d");
-  let fps = 8;
-  let index = 0;
-
-  if (frames["0.png"]) {
-    $canvas.width = frames["0.png"].frame.w;
-    $canvas.height = frames["0.png"].frame.h;
-  } else {
-    console.log(`缺少初始帧 0.png`);
   }
-
-  let interval = setInterval(function() {
-    ctx.clearRect(0, 0, $canvas.width, $canvas.height);
-    let info = frames[`${index}.png`].frame;
-    ctx.drawImage(
-      imgDom,
-      info.x,
-      info.y,
-      info.w,
-      info.h,
-      0,
-      0,
-      $canvas.width,
-      $canvas.height
-    );
-    index = index < framesNum - 1 ? index + 1 : 0;
-  }, 1000 / fps);
-};
-
-function loadJson($jsonPath) {
-  return new Promise((resolve, reject) => {
-    let request = new XMLHttpRequest();
-    request.open("get", $jsonPath);
-    request.send(null);
-    request.onload = function() {
-      if (request.status == 200) {
-        /*返回状态为200，即为数据获取成功*/
-        resolve(request.responseText);
-      } else {
-        reject();
-      }
-    };
-  });
-}
-
-function loadImg($dom, $imgUrl) {
-  return new Promise((resolve, reject) => {
-    $dom.onload = function() {
-      resolve();
-    };
-    $dom.onerror = function() {
-      reject(new Error(`无法找到图片${$imgUrl}`));
-    };
-    $dom.src = $imgUrl;
-  });
 }
